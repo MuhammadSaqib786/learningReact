247231943ec5f458a301663ab557f5cf2ee0af78
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-06-04 00:52:09 -0700

    add image

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
      <h1>Welcome</h1>
+     <img src="saqi.jpg" width="200px" height="200px"/>
      <br/>Hello world from saqib kayani and mujeeb
     </div>
   );
