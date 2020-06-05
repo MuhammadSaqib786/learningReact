9766df7f42faf29772090950a6f282c0a03cf210
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-06-04 21:21:49 -0700

    ADDED TABLE

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 function App() {
   return (
     <div>
-     <h1>Welcome</h1>
-     <img src="saqi.jpg" width="200px" height="200px" alt="saqib"/>
-     <br/>Hello world from <h1>saqib kayani</h1>  and <h2>mujeeb kayani</h2>
-    </div>
+      <table border="1" align="center">
+<tr>
+  <th>Name </th> <th>RollNo </th>
+  </tr>
+  <tr>
+    <td>Muhammad Saqib </td><td>PIAIC-53984 </td>
+    </tr>
+        </table>
+        </div>
   );
 }
 
